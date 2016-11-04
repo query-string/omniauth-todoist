503969005e0178e7d23862521f3f5d29b5d5fe0f
Alex Timofeev <atimofeev@reactant.ru>
2016-11-04 12:41:09 +0700

    Initialize gem

---
+++ lib/omniauth/todoist.rb
@@ -0,0 +1,7 @@
+require "omniauth/todoist/version"
+
+module Omniauth
+  module Todoist
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/todoist/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Todoist
+    VERSION = "0.1.0"
+  end
+end
