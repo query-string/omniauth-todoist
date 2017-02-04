0816661c5e664ff4977dc8b6e764d9c8d5b9f227
Alex Timofeev <atimofeev@reactant.ru>
2017-02-04 15:11:44 +0700

    Add some minor specs

---
+++ spec/omniauth/strategies/todoist_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe OmniAuth::Strategies::Todoist do
+  before do
+    OmniAuth.config.test_mode = true
+  end
+
+  after do
+    OmniAuth.config.test_mode = false
+  end
+
+  subject do
+    OmniAuth::Strategies::Todoist.new({})
+  end
+
+  context "client options" do
+    it "should have correct name" do
+      expect(subject.options.name).to eq("todoist")
+    end
+
+    it "should have correct site" do
+      expect(subject.options.client_options.site).to eq("https://todoist.com/")
+    end
+
+    it "should have correct authorize url" do
+      expect(subject.options.client_options.authorize_url).to eq("https://todoist.com/oauth/authorize")
+    end
+
+    it "should have correct token url" do
+      expect(subject.options.client_options.token_url).to eq("https://todoist.com/oauth/access_token")
+    end
+  end
+end
